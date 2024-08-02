1a6cc520021ad6b95313855dd7e63c64cb38e6f7
Narciso Correia <Narciso.Correia@ctw.bmwgroup.com>
2024-08-02 09:43:57 +0100

    Add New Functions

--- mavengpm/src/main/java/com/example/StringUtils.java
+++ mavengpm/src/main/java/com/example/StringUtils.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class StringUtils {
-    public static String reverse(String input) {
+    public static String reverseStringInator(String input) {
         return new StringBuilder(input).reverse().toString();
+    }
+
+    public static String greatingInator(String input) {
+        return "Hello From the Stitchers"
     }
 }
